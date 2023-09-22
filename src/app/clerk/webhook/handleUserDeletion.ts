5260184795c6c59416bb5a5409e75220f3376d7f
ohager <oliver@devbutze.com>
2023-09-22 08:25:24 -0300

    feat: clerk user webhooks

--- src/app/clerk/webhook/handleUserDeletion.ts
+++ src/app/clerk/webhook/handleUserDeletion.ts
@@ -3,12 +3,13 @@ import db from  '@/back/prisma'
 
 export async function handleUserDeletion(event: UserWebhookEvent) {
     const user = event.data as UserJSON;
+    console.log("Marking user as deleted:", user.email_addresses[0].email_address);
     await db.user.update({
         where: {
             email: user.email_addresses[0].email_address,
         },
         data: {
-            status: "Inactive",
+            status: "Deleted",
         }
     })
 }
--- src/app/clerk/webhook/handleUserUpdate.ts
+++ src/app/clerk/webhook/handleUserUpdate.ts
@@ -3,6 +3,7 @@ import db from  '@/back/prisma'
 
 export async function handleUserUpdate(event: UserWebhookEvent) {
     const user = event.data as UserJSON;
+    console.log("Updating user:", user.email_addresses[0].email_address);
     await db.user.update({
         where: {
             email: user.email_addresses[0].email_address,
